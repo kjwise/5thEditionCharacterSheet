14f0739bafcce042dc2f57a3dfb2e91f199761f7
Haffi <hafthor.bryndisarson@gmail.com>
2014-11-03 23:55:37 +0000

    Adding another character data file for testing.

---
+++ morn.js
@@ -0,0 +1,42 @@
+var charJson = 
+{
+  "name": "Morn",
+  "classes": [
+    {"Ranger": 2}
+    ],
+  "age": 27,
+  "height": "5\"9",
+  "weight": "138 lbs",
+  "eyes": "brown",
+  "skin": "tan",
+  "hair": "brown",
+  "hitdice": "1d10",
+  "background": "Outlander",
+  "race": "Human",
+  "alignment": "Chaotic Good",
+  "hitpoints": 20,
+  "playername": "Haffi",
+  "xp": null,
+  "abilityscores": {
+    "str": 14,
+    "dex": 16,
+    "con": 14,
+    "int": 11,
+    "wis": 16,
+    "cha": 9
+    },
+  "savingthrow_proficiencies":[
+    "str", "dex"
+  ],
+  "skill_proficiences":[
+    "athletics",
+    "insight",
+    "perception",
+    "stealth",
+    "survival"
+  ],
+  "hitpoints": 20,
+  "inspiration": false,
+  "ac": 14,
+  "speed": 30
+};
