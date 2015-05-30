b6ec2d2b635d39f2777660665a252b55f5acfc2d
Björn Leví <bjornlevi@gmail.com>
2015-05-30 23:10:31 +0000

    adding dagur.js

---
+++ dagur.js
@@ -0,0 +1,126 @@
+var charJson =
+{
+  "name": "Sken",
+  "classes": {
+    "Paladin": {
+      "level": 5,
+      "hitdice": "D10"
+    }
+  },
+  "inspirationpoints": 0,
+  "hitpoints": 57,
+  "ac": 19,
+  "speed": 30,
+  "mainattributes": {
+    "background": "Soldier - Blacksmith",
+    "race": "Half-Orc",
+    "alignment": "Chaotic Neutral",
+    "playername": "Dagur",
+    "xp": null
+  },
+  "appearance": {
+    "age": 27,
+    "height": "5\"7",
+    "weight": "240 lbs",
+    "eyes": "green",
+    "skin": "gray",
+    "hair": "black"
+  },
+  "abilityscores": {
+    "str": 16,
+    "dex": 14,
+    "con": 16,
+    "int": 10,
+    "wis": 10,
+    "cha": 14
+  },
+  "savingthrow_proficiencies":[
+    "wis", "cha"
+  ],
+  "skill_proficiences":[
+    "intimidation",
+    "athletics",
+    "stealth", 
+    "insight",
+    "persuasion"
+  ],
+  "attacks": {
+    "Warhammer": {
+      "ability": "str",
+      "die": "1d8/1d10",
+      "proficient": true,
+      "type": "b",
+      "extraAttackMod" : 0,
+      "extraDmgMod" : 0
+    },
+    "2x javelin": {
+      "ability": "str",
+      "die": "1d6",
+      "proficient": true,
+      "type": "p"
+    }
+  },
+  "equipment": {
+    "coins": {
+      "copper": 0,
+      "silver": 0,
+      "electrum": 0,
+      "gold": 680,
+      "platinium": 0
+    },
+    "armor": [
+      "Breastplate",
+      "Shield"
+    ],
+    "other": [
+      "Explorer's pack",
+      "Holy Symbol - War",
+      "A tiny chest carved to look like it has numerous feet on the bottom"
+    ]
+  },
+  "personality": {
+    "traits": "I can stare down a hell hound without flinching.",
+    "ideals": "Greater good - My life is forfeit in the destruction of the greater evil.",
+    "bonds": "Those who fight besides me are those worth dying for",
+    "flaws": "I'd rather eat my armor than admit I'm wrong."
+  },
+  "features": [
+    "Darkvision",
+    "Relentless Endurance",
+    "Savage Attack",
+    "Divine Sense",
+    "Lay on Hands",
+    "Defense",
+    "Spellcasting",
+    "Divine Smite",
+    "Divine Health",
+    "Sacret Oath of Vengeance:",
+    "Fight the Greater Evil",
+    "No Mercy for the Wicket",
+    "By any Means Necessary",
+    "Restitution",
+    "Extra Attack",
+    "Military Rank"
+  ],
+  "spell slots": {
+    "cantrips": 0,
+    "1st level": 4,
+    "2nd level": 2
+  },
+  "spells": {
+    "cantrips": [],
+    "1st level": ["Compelled Duel", "Shield of Faith", "Hunter's Mark"],
+    "2nd level": ["Branding Strike"]
+  },
+  "proficiencies": [
+    "Blacksmith's tools",
+    "Three dragon ante set",
+    "Simple & martial weapons",
+    "Light, medium & heavy armor",
+    "Vehicles (land)"
+  ],
+  "languages": [
+    "Common",
+    "Orc"
+  ]
+};
