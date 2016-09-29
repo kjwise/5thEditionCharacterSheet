af95b4bc2e9057b698df77fb9fe05509ac1963dd
Jóhann Haukur Gunnarsson <kjwise@gmail.com>
2016-09-29 10:28:23 +0000

    Adding initial version of Mehen Marr Magus, a level 1 Paladin

---
+++ magus.js
@@ -0,0 +1,110 @@
+var charJson =
+{
+    "name": "Mehen Marr Magus",
+    "classes": {
+        "Paladin": {
+            "level": 1,
+            "hitdice": "d10"
+        }
+    },
+    "inspirationpoints": 0,
+    "hitpoints": 12,
+    "ac": 18,
+    "speed": 30,
+    "mainattributes": {
+        "background": "FA - Arcane Brotherhood",
+        "race": "Dragonborn",
+        "alignment": "Neutral Good",
+        "playername": "Jói",
+        "xp": 0
+    },
+    "appearance": {
+        "age": 15,
+        "height": "6\'8\"",
+        "weight": "260 lbs.",
+        "eyes": "gold",
+        "skin": "silver",
+        "hair": "silver"
+    },
+    "abilityscores": {
+        "str": 16, // +2 Dragonborn
+        "dex": 12,
+        "con": 15,
+        "int": 8,
+        "wis": 9,
+        "cha": 16 // +1 Dragonborn
+    },
+    "savingthrow_proficiencies": [
+        "wis",
+        "cha"
+    ],
+    "skill_proficiences": [
+        "athletics",
+        "persuasion",
+        "insight",
+        "religion"
+    ],
+    "attacks": {
+        "Warhammer": {
+            "ability": "str",
+            "extraDmgMod": +0,
+            "die": "1d8",
+            "proficient": true,
+            "type": "bludgeon"
+        },
+        "Javelins": {
+            "ability": "str",
+            "extraDmgMod": +0,
+            "die": "1d6",
+            "proficient": true,
+            "type": "piercing"
+        }
+    },
+    "equipment": {
+        "coins": {
+            "copper": 0,
+            "silver": 0,
+            "electrum": 0,
+            "gold": 15,
+            "platinum": 0
+        },
+        "other": [
+            "A rank insignia from a lost legioneer",
+            "Explorer's pack",
+            "Traveler\'s clothes",
+            "Holy symbol of Mystra",
+            "Symbol of the Arcane Brotherhood",
+            "Longsword",
+            "Shield",
+            "Javelins",
+            "Chain mail",
+            "Battle axe"
+        ]
+    },
+    "personality": {
+        "traits": "I see omens in every event and action.",
+        "ideals": "I always try to help those in need, no matter the personal cost.",
+        "bonds": "The Arcane Brotherhood is my clan.",
+        "flaws": "I put too much trust in those that wield power within the Arcane Brotherhood."
+    },
+    "features": [
+        "Silver dragon ancestry: Cold resistance",
+        "Breath Weapon 2d6 cold dmg: Cold cone 15 ft Con save (8 +prof +con mod) 1 per rest.",
+        "Divine Sense: Action to detect good and evil 1+ cha mod per long rest",
+        "Lay on hands",
+        "FEAT - Magic Initiate Warlock: Hex, El-blast & Greenflame blade"
+    ],
+    "proficiencies": [ 
+        "All armor and shields",
+        "Simple & martial weapons"
+    ],
+    "languages": [
+        "Common",
+        "Draconic",
+        "Elvish",
+        "Primordial"
+    ],
+    "contacts": [
+        "Someone someone Arcane Brotherhood"
+    ]
+};
